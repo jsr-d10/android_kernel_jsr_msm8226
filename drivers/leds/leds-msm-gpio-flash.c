6253b089eadedc163e59d90e091b8c09979825bb
remittor <remittor@gmail.com>
2015-02-28 13:05:17 +0300

    leds: Add kernel wake lock "torch_wake_lock" (from stock) CONFIG_JSR_TORCH_WAKE_LOCK=y Used disasm IDA.

--- drivers/leds/leds-msm-gpio-flash.c
+++ drivers/leds/leds-msm-gpio-flash.c
@@ -20,6 +20,7 @@
 #include <linux/gpio.h>
 #include <linux/of.h>
 #include <linux/printk.h>
+#include <linux/wakelock.h>
 
 #define LED_GPIO_FLASH_DRIVER_NAME	"qcom,leds-gpio-flash"
 #define LED_TRIGGER_DEFAULT		"none"
@@ -28,6 +29,9 @@ struct led_gpio_flash_data {
 	int flash_en;
 	int flash_now;
 	int brightness;
+#ifdef CONFIG_JSR_TORCH_WAKE_LOCK
+	struct wake_lock torch_wake_lock;
+#endif
 	struct led_classdev cdev;
 };
 
@@ -35,6 +39,10 @@ static struct of_device_id led_gpio_flash_of_match[] = {
 	{.compatible = LED_GPIO_FLASH_DRIVER_NAME,},
 	{},
 };
+
+#ifdef CONFIG_JSR_TORCH_WAKE_LOCK
+static int torch_wake_lock_flag = 0;
+#endif
 
 static void led_gpio_brightness_set(struct led_classdev *led_cdev,
 				    enum led_brightness value)
@@ -46,6 +54,27 @@ static void led_gpio_brightness_set(struct led_classdev *led_cdev,
 	int brightness = value;
 	int flash_en = 0, flash_now = 0;
 
+#ifdef CONFIG_JSR_TORCH_WAKE_LOCK	
+	if (brightness >= LED_FULL) {	  
+		if (torch_wake_lock_flag == 0x55)
+			wake_unlock(&flash_led->torch_wake_lock);
+		flash_en = 1;
+		flash_now = 1;
+		torch_wake_lock_flag = 0;
+	} else
+	if (brightness >= LED_HALF) {
+		flash_en = 1;
+		flash_now = 0;
+		torch_wake_lock_flag = 0x55;
+		wake_lock(&flash_led->torch_wake_lock);
+	} else {
+		if (torch_wake_lock_flag == 0x55)
+			wake_unlock(&flash_led->torch_wake_lock);
+		flash_en = 0;
+		flash_now = 0;
+		torch_wake_lock_flag = 0;	
+	}
+#else
 	if (brightness > LED_HALF) {
 		flash_en = 0;
 		flash_now = 1;
@@ -56,6 +85,7 @@ static void led_gpio_brightness_set(struct led_classdev *led_cdev,
 		flash_en = 0;
 		flash_now = 0;
 	}
+#endif
 
 	rc = gpio_direction_output(flash_led->flash_en, flash_en);
 	if (rc) {
@@ -103,6 +133,10 @@ int __devinit led_gpio_flash_probe(struct platform_device *pdev)
 	if (!rc)
 		flash_led->cdev.default_trigger = temp_str;
 
+#ifdef CONFIG_JSR_TORCH_WAKE_LOCK
+	wake_lock_init(&flash_led->torch_wake_lock, WAKE_LOCK_SUSPEND, "torch_wake_lock");
+#endif
+	
 	flash_led->flash_en = of_get_named_gpio(node, "qcom,flash-en", 0);
 
 	if (flash_led->flash_en < 0) {
@@ -166,6 +200,9 @@ int __devinit led_gpio_flash_probe(struct platform_device *pdev)
 	return 0;
 
 error:
+#ifdef CONFIG_JSR_TORCH_WAKE_LOCK
+	wake_lock_destroy(&flash_led->torch_wake_lock);
+#endif
 	devm_kfree(&pdev->dev, flash_led);
 	return rc;
 }
@@ -175,6 +212,9 @@ int __devexit led_gpio_flash_remove(struct platform_device *pdev)
 	struct led_gpio_flash_data *flash_led =
 	    (struct led_gpio_flash_data *)platform_get_drvdata(pdev);
 
+#ifdef CONFIG_JSR_TORCH_WAKE_LOCK
+	wake_lock_destroy(&flash_led->torch_wake_lock);
+#endif
 	led_classdev_unregister(&flash_led->cdev);
 	devm_kfree(&pdev->dev, flash_led);
 	return 0;
